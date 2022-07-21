0cdce891f77b4a91c6371e301e4a7ee5e87e2e6f
Peter O Sullivan <peter@peterosullivan.net>
2022-07-21 14:37:41 +1000

    quote url's to avoid curl errors

--- multiroom.sh
+++ multiroom.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export MASTER=192.168.0.30
 export GUEST=192.168.0.31
-curl -X GET $MASTER/httpapi.asp?command=setPlayerCmd:switchmode:line-in /echo -H "Accept: */*"
-curl -X GET $GUEST/httpapi.asp?command=ConnectMasterAp:JoinGroupMaster:eth$MASTER /echo -H "Accept: */*"
+
+curl -X GET "$MASTER/httpapi.asp?command=setPlayerCmd:switchmode:line-in" /echo -H "Accept: */*"
+curl -X GET "$GUEST/httpapi.asp?command=ConnectMasterAp:JoinGroupMaster:eth$MASTER" /echo -H "Accept: */*"
